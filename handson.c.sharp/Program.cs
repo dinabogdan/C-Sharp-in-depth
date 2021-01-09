8adea71da3d49d12bb34e87a26d929c2c68b425c
Dina Bogdan <dinabogdan@192-168-0-128.rdsnet.ro>
2021-01-09 14:23:48 +0200

    initial commit with project structure

---
+++ handson.c.sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace handson.c.sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
