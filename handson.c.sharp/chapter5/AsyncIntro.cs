3f84f02c275fb73c49e5d50665e6e5583a525c5d
dinabogdan <bogdan.dina03@gmail.com>
2021-02-05 12:45:08 +0200

    simple example about throwing exceptions in async functions

--- handson.c.sharp/chapter5/AsyncIntro.cs
+++ handson.c.sharp/chapter5/AsyncIntro.cs
@@ -28,7 +28,17 @@ namespace handson.c.sharp.chapter5
             Console.WriteLine("After second await");
         }
 
-        static void Main()
+        static async Task<int> ComputeLengthAsync(string text)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException("text");
+            }
+            await Task.Delay(500);
+            return text.Length;
+        }
+
+        static async Task Main()
         {
             AsyncIntro asyncIntro = new AsyncIntro();
             asyncIntro.DisplayWebSiteLength().Wait();
@@ -38,6 +48,11 @@ namespace handson.c.sharp.chapter5
             Console.WriteLine("method returned");
             task.Wait();
             Console.WriteLine("Task completed");
+
+            Task<int> computedLengthTask = ComputeLengthAsync(null);
+            Console.WriteLine("fetched the task");
+            int length = await computedLengthTask;
+            Console.WriteLine("Length: {0}", length);
         }
     }
 }
