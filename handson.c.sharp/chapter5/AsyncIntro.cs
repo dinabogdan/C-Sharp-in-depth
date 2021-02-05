75204b389882cd9ce2d6edec3e9802e1670af455
dinabogdan <bogdan.dina03@gmail.com>
2021-02-05 12:01:24 +0200

    simple async-await example

--- handson.c.sharp/chapter5/AsyncIntro.cs
+++ handson.c.sharp/chapter5/AsyncIntro.cs
@@ -19,10 +19,25 @@ namespace handson.c.sharp.chapter5
             Console.WriteLine(text);
         }
 
+        static async Task DemoCompletedAsync()
+        {
+            Console.WriteLine("Before first await");
+            await Task.FromResult(10);
+            Console.WriteLine("Between awaits");
+            await Task.Delay(1000);
+            Console.WriteLine("After second await");
+        }
+
         static void Main()
         {
             AsyncIntro asyncIntro = new AsyncIntro();
             asyncIntro.DisplayWebSiteLength().Wait();
+
+
+            Task task = DemoCompletedAsync();
+            Console.WriteLine("method returned");
+            task.Wait();
+            Console.WriteLine("Task completed");
         }
     }
 }
